f105b62f401a42f98fbbabebd2082e599f689a2f
2023121068 <2023121068@LAB2-001>
2023-11-07 17:01:02 -0300

    Adicionar arquivos de projeto.

---
+++ Teste_Conceitos_Basicos/UnitTest1.cs
@@ -0,0 +1,72 @@
+using Conceitos_Basicos;
+
+namespace Teste_Conceitos_Basicos
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private int num1, num2, resultado;
+        [TestMethod]
+        public void TestaSomaDoisNumerosInteros()
+        {
+            //Cenario
+            InicializaVariaveis();
+            Calculadora calculadora = new Calculadora();
+
+            //Ação
+            resultado = calculadora.Soma(num1, num2);
+
+            //Verificação
+            Assert.AreEqual(50, resultado);
+
+
+        }
+        [TestMethod]
+        public void TestaSubtracaoDoisNumerosInteiros()
+        {
+            //Cenario
+            InicializaVariaveis();
+
+            var calculadora = new Calculadora();
+
+            //Ação
+            resultado = calculadora.Subtracao(num1, num2);
+
+            //Verificação
+            Assert.AreEqual(-10, resultado);
+        }
+
+        [TestMethod]
+        public void VerificaNumeroPar()
+        {
+            //Cenario e Ação
+            bool verifica = Calculadora.VerificaImpar(num1);
+            InicializaVariaveis();
+
+            //Verificação
+            Assert.IsFalse(verifica);
+
+        }
+
+        [TestMethod]
+        public void VerificaNumeroImpar()
+        {
+            //Cenario e Ação
+            bool verifica = Calculadora.VerificaImpar(++num1);
+            InicializaVariaveis();
+
+            //Verificação
+            Assert.IsTrue(verifica);
+
+        }
+
+        private void InicializaVariaveis()
+        {
+            num1 = 20;
+            num2 = 30;
+        }
+    }
+}
+
+// Teste para retornar o fatorial de um Número
+//Teste para saber se um número é primo
